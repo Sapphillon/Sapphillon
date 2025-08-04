d794a278e30760800f1e55c6f22f5b0008f6612e
Yuta Takahashi <y.highbridge04@gmail.com>
2025-08-04 11:56:50 +0000

    Add WorkflowResult fields and WorkflowResultType enum for enhanced workflow result representation

--- src/proto_generated/sapphillon.v1.rs
+++ src/proto_generated/sapphillon.v1.rs
@@ -154,6 +154,17 @@ pub struct WorkflowResult {
     pub result: ::prost::alloc::string::String,
     #[prost(message, optional, tag="5")]
     pub ran_at: ::core::option::Option<::prost_types::Timestamp>,
+    #[prost(enumeration="WorkflowResultType", tag="6")]
+    pub result_type: i32,
+    /// Exit code of the workflow execution
+    #[prost(int32, tag="7")]
+    pub exit_code: i32,
+    /// Revision of the workflow code that produced this result
+    #[prost(int32, tag="8")]
+    pub workflow_code_revision: i32,
+    /// Revision of the workflow result
+    #[prost(int32, tag="9")]
+    pub workflow_result_revision: i32,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -180,6 +191,7 @@ pub struct Workflow {
 pub enum WorkflowLanguage {
     Unspecified = 0,
     Typescript = 1,
+    Javascript = 2,
 }
 impl WorkflowLanguage {
     /// String value of the enum field names used in the ProtoBuf definition.
@@ -190,6 +202,7 @@ impl WorkflowLanguage {
         match self {
             WorkflowLanguage::Unspecified => "WORKFLOW_LANGUAGE_UNSPECIFIED",
             WorkflowLanguage::Typescript => "WORKFLOW_LANGUAGE_TYPESCRIPT",
+            WorkflowLanguage::Javascript => "WORKFLOW_LANGUAGE_JAVASCRIPT",
         }
     }
     /// Creates an enum from field names used in the ProtoBuf definition.
@@ -197,6 +210,33 @@ impl WorkflowLanguage {
         match value {
             "WORKFLOW_LANGUAGE_UNSPECIFIED" => Some(Self::Unspecified),
             "WORKFLOW_LANGUAGE_TYPESCRIPT" => Some(Self::Typescript),
+            "WORKFLOW_LANGUAGE_JAVASCRIPT" => Some(Self::Javascript),
+            _ => None,
+        }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum WorkflowResultType {
+    SuccessUnspecified = 0,
+    Failure = 1,
+}
+impl WorkflowResultType {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            WorkflowResultType::SuccessUnspecified => "WORKFLOW_RESULT_TYPE_SUCCESS_UNSPECIFIED",
+            WorkflowResultType::Failure => "WORKFLOW_RESULT_TYPE_FAILURE",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "WORKFLOW_RESULT_TYPE_SUCCESS_UNSPECIFIED" => Some(Self::SuccessUnspecified),
+            "WORKFLOW_RESULT_TYPE_FAILURE" => Some(Self::Failure),
             _ => None,
         }
     }
