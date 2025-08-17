34316229e9884909759fc297c32cb4f21a9336fe
Yuta Takahashi <y.highbridge04@gmail.com>
2025-08-17 14:22:55 +0000

    Remove unused add function and its test from fetch plugin

--- plugins/fetch/src/lib.rs
+++ plugins/fetch/src/lib.rs
@@ -11,22 +11,10 @@ fn fetch(url: &str) -> Result<String> {
 }
 
 
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-    
     #[test]
     fn test_fetch() {
         let url = "https://dummyjson.com/test";
