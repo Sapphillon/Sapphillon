ffd918a48a6d4c4e7edb609d712e4c4ecb15c263
Yuta Takahashi <y.highbridge04@gmail.com>
2025-08-20 05:12:04 +0000

    feat: add workflow module and update services to include it

--- src/services/workflow.rs
+++ src/services/workflow.rs
@@ -16,9 +16,4 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// Service root module
 
-mod version;
-mod workflow;
-pub use version::*;
-pub use workflow::*;
