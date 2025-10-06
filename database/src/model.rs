f3dcefc49773c68bbd00d3fce79127fdd0425018
Yuta Takahashi <y.highbridge04@gmail.com>
2025-10-06 05:12:15 +0000

    feat: add model module to define data structures and ORM interactions

--- database/src/model.rs
+++ database/src/model.rs
@@ -16,5 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub mod provider;
-pub mod model;
+use entity::entity::model;
+use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr, EntityTrait, QuerySelect};
+
