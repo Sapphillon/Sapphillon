da961d61fdf1c4355877d46b7bf676ab31be688c
Yuta Takahashi <y.highbridge04@gmail.com>
2025-10-12 04:58:58 +0000

    feat: add new plugin module to the database

--- database/src/plugin.rs
+++ database/src/plugin.rs
@@ -14,10 +14,5 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
 
-pub mod model;
-pub mod provider;
-pub mod permission;
-pub mod workflow;
-pub mod plugin;
