4ebcd430245b7292663f911cb7f123fa571e4fa8
Yuta Takahashi <y.highbridge04@gmail.com>
2026-01-11 13:42:03 +0000

    feat: Temporarily suspend Windows support in CI workflows and build script

--- build.rs
+++ build.rs
@@ -3,5 +3,9 @@
 // SPDX-License-Identifier: MPL-2.0 OR GPL-3.0-or-later
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO Resuport Windows
+    #[cfg(target_os = "windows")]
+    unimplemented!("Currently, Windows Support is suspended.");
+
     Ok(())
 }
